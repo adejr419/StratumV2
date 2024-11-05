b8a64d43e0c09837f6a42ac9a60ca02377dca549
jbesraa <jbesraa@gmail.com>
2024-11-05 17:13:08 +0200

    Rewrite mining protocol messages docs

--- protocols/v2/subprotocols/mining/src/close_channel.rs
+++ protocols/v2/subprotocols/mining/src/close_channel.rs
@@ -6,21 +6,22 @@ use binary_sv2::{Deserialize, Serialize, Str0255};
 #[cfg(not(feature = "with_serde"))]
 use core::convert::TryInto;
 
-/// # CloseChannel (Client -> Server, Server -> Client)
+/// Use by downstream role to announce end of operation.
 ///
-/// Client sends this message when it ends its operation. The server MUST stop sending messages
-/// for the channel. A proxy MUST send this message on behalf of all opened channels from a
-/// downstream connection in case of downstream connection closure.
+/// A proxy must send this message on behalf of all opened channels from a downstream connection in
+/// case of downstream connection closure.
+///
+/// Upon receiving this message, upstream must stop sending messages for the channel.
 ///
 /// If a proxy is operating in channel aggregating mode (translating downstream channels into
-/// aggregated extended upstream channels), it MUST send an UpdateChannel message when it
-/// receives CloseChannel or connection closure from a downstream connection. In general, proxy
-/// servers MUST keep the upstream node notified about the real state of the downstream
-/// channels.
+/// aggregated extended upstream channels), it must send an [`UpdateChannel`] message when it
+/// receives [`CloseChannel`] or connection closure from a downstream connection. In general, a
+/// proxy must keep the upstream node notified about the real state of the downstream channels.
 ///
+/// [`UpdateChannel`]: crate::UpdateChannel
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct CloseChannel<'decoder> {
-    /// Channel identification.
+    /// Channel id of the channel to be closed.
     pub channel_id: u32,
     /// Reason for closing the channel.
     #[cfg_attr(feature = "with_serde", serde(borrow))]
